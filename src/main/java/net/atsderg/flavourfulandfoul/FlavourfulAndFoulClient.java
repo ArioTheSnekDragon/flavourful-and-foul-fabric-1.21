f8b47d25a7e989c36acc4cbb1726444ad471b93c
ArioTheSnekDragon <joniovine45@gmail.com>
2024-11-05 23:00:59 +0800

    Initial commit

---
+++ src/main/java/net/atsderg/flavourfulandfoul/FlavourfulAndFoulClient.java
@@ -0,0 +1,10 @@
+package net.atsderg.flavourfulandfoul;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlavourfulAndFoulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/atsderg/flavourfulandfoul/FlavourfulAndFoulDataGenerator.java
@@ -0,0 +1,11 @@
+package net.atsderg.flavourfulandfoul;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlavourfulAndFoulDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
